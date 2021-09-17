58a8dfeafb8ec1200013118d12e8acdf8f0cf58e
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-17 16:55:04 +0530

    Using conventional css to style the article.

--- reactlearnings/src/index.js
+++ reactlearnings/src/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 function Booklist() {
   return (
-  <>
+  <section className="booklist">
   <Book />
   <Book />
-  </>
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  <Book />
+  </section>
   );
 }
 
-const Book = () => {return(<> 
-   <Image />
+const Book = () => {return(
+<article className="book"> 
+  <Image />
   <Title />
   <Author /> 
-  </> );
+</article> );
 }
 
 const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" alt="Héctor García " />;
