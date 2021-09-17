381eceb561950e13d2f93d92a6bd20691b3fc372
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-17 16:35:29 +0530

    Added mini-book project "to see the real use of components"

--- reactlearnings/src/index.js
+++ reactlearnings/src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Greetings(){
+function Booklist() {
   return (
-    <>
-    <Person />
-    <p>This is my message!</p>
-    </>
+  <>
+  <Book />
+  <Book />
+  </>
   );
 }
 
-const Person = () => <h2>Mahendra Kumar,</h2>;
+const Book = () => {return(<> 
+   <Image />
+  <Title />
+  <Author /> 
+  </> );
+}
 
+const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" alt="Héctor García " />;
+const Title = () => <h3>Ikigai: The Japanese secret to a long and happy life</h3>;
+const Author = () => <h3>Héctor García </h3>;
 
-
-ReactDOM.render(
-  <Greetings/>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Booklist />, document.getElementById("root"));
