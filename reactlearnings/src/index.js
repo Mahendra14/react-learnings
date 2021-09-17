9b69fa2d761a84576fdcd5bbf0da3d42835c0e58
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-17 13:03:23 +0530

    Using create element aka under the hood.

--- reactlearnings/src/index.js
+++ reactlearnings/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Greetings(){
-  return <h4>My first Component!</h4>;
+// function Greetings(){
+//   return <h4>My first Component!</h4>;
+// }
+
+const Greeting = () => {
+  return React.createElement('h1',{},'Hello mahendra!');
 }
 
 
 ReactDOM.render(
-  <Greetings />,
+  <Greeting />,
   document.getElementById('root')
 );
 
