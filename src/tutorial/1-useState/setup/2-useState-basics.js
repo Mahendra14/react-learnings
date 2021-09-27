5b484732e6837a279578fc3a73d9384bbadf1111
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-27 13:42:43 +0530

    using useState basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [value,setValue] = useState('random value');
+  const handleButton = () => {
+    if(value === "random value"){
+    setValue("hello world");
+    }
+    else{
+      setValue("random value");
+    }
+  }
+  return (<> 
+  <h3>{value}</h3>
+  <button className="btn" onClick={handleButton}>change value</button>
+  
+  </>);
 };
 
 export default UseStateBasics;
