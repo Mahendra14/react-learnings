8ca9fc9f238153de7fbe27b3af378cf65e3848d1
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-27 14:46:08 +0530

    Used array of useState in this one.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className='container'>
@@ -9,3 +9,5 @@ function App() {
 }
 
 export default App
+
+//THING TO NOTE WHEN COMPARING ARRAYS JUST JUST COMPARE THE LENGHT IS 0 OR NOT ONLY WAY TO DO IT.
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,51 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people,setPeople] = React.useState(data);
+  const [buttonName, setButtonName] = React.useState('clear members');
+  const removeItem = (id) => {
+    const newPeople = people.filter((item) => item.id !== id )
+    setPeople(newPeople);
+  }
+
+  React.useEffect(() => {
+    console.log("hello");
+    console.log(people);
+    if(people.length !== 0){
+      setButtonName('clear members');
+    }
+    else{
+      setButtonName('add members');
+    }
+  }, [people])
+
+  const toggleData = () => {
+    if(people.length !== 0){
+    setPeople([]);
+    return ;
+    }
+    else{
+      setPeople(data);
+    }
+  }
+
+  return (
+    <>
+    {
+        people.map((person) => {
+        const  { id, name } = person;
+        return (
+          <div className="item" key={id}>
+            <h4>{name}</h4>
+            {/*So here we are just trying to only call the function when clicked by passing id */}
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })
+    }
+    <button className="btn" onClick = {toggleData}>{buttonName}</button>
+    </>
+  );
 };
 
 export default UseStateArray;
