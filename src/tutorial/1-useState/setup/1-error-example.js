4077b2ee4cc2b55c6ded738fcc96473ea9faee75
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-27 13:39:21 +0530

    why to use UseState hook

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let valueDisplay = "Random Value";
+  const handleButton = () => {
+    valueDisplay = "hello world";
+    console.log(valueDisplay);
+    //here we are changing the value of the variable because there is no rerendering its not reflected.
+  }
+  return(<>
+  <h2>{valueDisplay}</h2>
+  <button className="btn" onClick={handleButton}>change button</button>
+   </>);
 };
 
 export default ErrorExample;
