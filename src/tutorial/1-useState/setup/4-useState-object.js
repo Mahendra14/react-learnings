67923ea462c4c8441c873a9e01c473e269796f72
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-01 12:08:01 +0530

    worked around with teh useState-object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  //its completely fine to use three diff useState instead of objects
+  const [person,setPerson] = useState({
+    name: 'Mahendra',
+    age: 21,
+    msg: 'I am from useState'
+  })
+
+  //defining the button handler 
+  const buttonHandler = () => {
+    setPerson({...person,msg:'Sorry Message is changed!'})
+  }
+  return (<>
+  <h3>{person.name}</h3>
+  <h3>{person.age}</h3>
+  <h3>{person.msg}</h3>
+  <button className='btn' onClick={buttonHandler}>change message</button>
+  </>);
 };
 
 export default UseStateObject;
