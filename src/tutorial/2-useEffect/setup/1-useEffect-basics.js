acecda2d1f83bc3c419e9f8286802a27dc9b28a4
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-03 12:38:12 +0530

    useEffect basis done

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
-// cleanup function
-// second parameter
+// by default runs after every re-render - point based on the useEffect
+// cleanup function - it can return a function which will be looking after that hence used normally for the sake of clean up process;
+// second parameter - to implicitly specify when to be executed the array can be given or by defaut every re-render 
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value,setValue] = useState(0);
+
+  // here one important thing to note is that we can write a useEffect inside if conditional or any othre where as we can write those conditionals inside the useEffect as belows
+  //we can also ommit this value in teh rendering array as every time the value changes anyway its rerendered
+  useEffect(() => {
+    if(value >= 1)
+    {document.title = `New message(${value})`}
+  },[value]);
+
+  //to increase the value of the useState after every button click
+  const buttonHandler = () => {
+    setValue(value+1);
+  }
+
+  return (
+    <>
+    <h1>{value}</h1>
+    <button className="btn" onClick={buttonHandler}>click me</button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
