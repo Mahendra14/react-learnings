b28811a8889b6e8d97b0bae066dcef2fa77117fc
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-03 14:09:06 +0530

    succesfully fetched the data and showcased(used flex)

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -5,13 +5,34 @@ const url = 'https://api.github.com/users';
 const UseEffectFetchData = () => {
   const [people,setPeople] = useState([]);
 
-  //using useEffect to fetch data from the api link. fetch api inbuilt
-  useEffect(() => {
+  const getUsers = async () =>{
+    const response = await fetch(url);
+    const users = await response.json();
+    setPeople(users);
+  }
 
+  //using useEffect to fetch data from the api link. fetch api inbuilt
+  //must be only during initial rendering other wise its just a loop and crashed for every change.
+  useEffect(() => {
+    getUsers();
   },[]);
   return (
     <>
     <h3>github users</h3>
+    <ul className='users'>    {people.map((user) => {
+       const { id, login, avatar_url, html_url } = user;
+      return (
+        <li key={id}>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h4>{login}</h4>
+          <a href={html_url}>profile</a>
+        </div>
+        </li>
+      );
+    })}
+    </ul>
+
     </>
   );
 };
