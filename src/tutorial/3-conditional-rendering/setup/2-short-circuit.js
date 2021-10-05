69c36bbf54d2288367f01ba3517c32acf7298203
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-05 17:29:54 +0530

    diff scenarios with || and && in short circuit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,26 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  //empty string is falsy..
+  const [text,setText] = useState('Mahi');
 
-  return <h2>short circuit</h2>;
+  //here if the text is true it returns text wheras if its false it will return teh second text(assuming second one is true)
+  //this is helpful to return default value if something may be empty
+  const firstValue = text || 'hello world';
+
+  //here its false if its true...the below one will return hello world
+  //here we are returning somehting if and only if the first one is true.
+  const secondValue = text && 'hello world';
+
+  return (<>
+    {/* <h2>{firstValue}</h2>
+    <h2>{secondValue}</h2> */}
+
+    <h1>{text || 'John Doe'}</h1>
+    <h1>{text && 'Mahendra'}</h1>
+    <h2>{!text && 'Mahedra'}</h2>
+    </>
+  );
 };
 
 export default ShortCircuit;
