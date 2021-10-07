ce509fb9e751d79fd92da686e7b62464c4c97c7a
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-07 11:07:00 +0530

    added useRef to the setup

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 
+
+
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const inputRef = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(inputRef.current);
+  }
+
+  return (
+    <div>
+      <form action="" className="form" onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef}/>
+        <button className="btn">Submit</button>
+
+      </form>
+    </div>
+  );
 };
 
 export default UseRefBasics;
