67189671604cc2823d4c10ce5df3e302693b1eaa
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-08 17:34:21 +0530

    handled close modal state

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -31,7 +31,9 @@ const reducer = (state,action) => {
   if(action.type === 'NO_VALUE'){
     return{ ...state,isModalOpen: true, modalContent: 'please enter a value'};
   }
-  if(action.type === 'CLOSE_MODAL'){}
+  if(action.type === 'CLOSE_MODAL'){
+    return {...state,isModalOpen: false};
+  }
   if(action.type === 'REMOVE_ITEM'){
     const newPeople = state.people.filter((person) => person.id !== action.payload);
     return {...state, people: newPeople};
