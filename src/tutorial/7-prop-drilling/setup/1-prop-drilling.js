c0abb5896c237e24a528885355ab31926f542f09
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-08 17:39:07 +0530

    some dev notes on prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 // more components
 // fix - context api, redux (for more complex cases)
 
+//demonstration that props that are sent over to diff components to pass through ones that doesnt use them its hecka work its just a demonstration to make our life simpler we use fix - context api, redux (for more complex cases)
+
 const PropDrilling = () => {
   return <h2>prop drilling</h2>;
 };
