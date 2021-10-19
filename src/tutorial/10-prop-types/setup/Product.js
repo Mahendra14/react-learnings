6f2fff8e4552622f535ba173c69d0c64f06e19dd
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-19 11:27:58 +0530

    added a normal || tag to handle the price problem

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import defImage from "../../../assets/default-image.jpeg"
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({name,image,price}) => {
+  // const imgSource = image.url || defimage;
+  return (
+  <article className='product'>
+    {/* <img src={imgSource} alt={name}/> */}
+    <h2>{name}</h2>
+    {/* making use of || operator to handle the case where price is missing */}
+
+    <p>${price || "3.99"}</p>
+
+  </article>);
 };
 
 export default Product;
