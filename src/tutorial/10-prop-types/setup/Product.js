5c0a8c20ca3bde6b1dac213906d6598d986395a3
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-20 17:26:41 +0530

    adding propTypes to the component "Product"

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import defImage from "../../../assets/default-image.jpeg"
 
 const Product = ({name,image,price}) => {
   // const imgSource = image.url || defimage;
+  const imgSource = image && image.url;
   return (
   <article className='product'>
-    {/* <img src={imgSource} alt={name}/> */}
-    <h2>{name}</h2>
+    <img src={imgSource || defImage} alt={name}/>
+    <h4>{name}</h4>
     {/* making use of || operator to handle the case where price is missing */}
 
     <p>${price || "3.99"}</p>
@@ -14,4 +16,18 @@ const Product = ({name,image,price}) => {
   </article>);
 };
 
+
+//defining the proptype for the component Product
+Product.protoTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+}
+
+//similarly we can also add default values to the propTypes
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage,
+// };
 export default Product;
