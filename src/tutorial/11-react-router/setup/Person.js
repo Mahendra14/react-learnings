6e45b651f1d2c228537eb2c6cdc3b97bef1f5b7d
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-11-02 10:08:02 +0530

    added a useEffect to get the data when page loads

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 // to access the id of the person we need the useParams hook 
 const Person = () => {
   // as we get back an object i will destructure it right away to get my id
   const {id} = useParams();
   const [name,setName] = useState("Default Name");
+
+  useEffect( () => {
+    const user = data.find((p) => p.id === parseInt(id));
+    setName(user.name);
+  },[]);
    return (
     <div>
       <h2>{name}</h2>
