277649f229077e378df09db497a970392c4d01d2
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-11-02 10:05:32 +0530

    added back to people and got the id from params

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
+
+// to access the id of the person we need the useParams hook 
 const Person = () => {
-  return (
+  // as we get back an object i will destructure it right away to get my id
+  const {id} = useParams();
+  const [name,setName] = useState("Default Name");
+   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to="/people" className="btn">Back to People</Link>
     </div>
   );
 };
