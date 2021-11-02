679caea5bd98a8d23649f34a2d255ad88d77df86
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-11-02 10:57:28 +0530

    using useMemo to memoise the function adn add the most expensive one

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -18,6 +18,17 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price
+      if (price >= total) {
+        total = price
+      }
+      return total
+    }, 0) / 100
+  )
+}
 
 const Index = () => {
   const { products } = useFetch(url)
@@ -28,12 +39,17 @@ const Index = () => {
     setCart(cart + 1)
   }, [cart])
 
+  const mostExpensive = useMemo(() => calculateMostExpensive(products), [
+    products,
+  ])
+
   return (
     <>
       <h1>Count : {count}</h1>
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
+      <h1>Most Expensive : ${mostExpensive}</h1>
       <BigList products={products} addToCart = {addToCart} />
     </>
   )
